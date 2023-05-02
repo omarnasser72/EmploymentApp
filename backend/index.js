d60013d72e69b4ce25101073d33cf15a5970f6f6
Maysara-nagy <maysara.nagy@gmail.com>
2023-05-02 17:37:28 +0200

    commited

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+index.js‏
 //--------------------intialize express app-------------------
 const express = require("express");
 const app = express();
@@ -17,6 +18,7 @@ const jobs = require("./routes/jobs");
 const qualfications = require("./routes/qualifications");
 const requests = require("./routes/requests");
 const applicants = require("./routes/Applicants");
+const ApplicantsM = require("./routes/ApplicantsM");
 //----------------run the app module-----------------
 app.listen(4000, "localhost", () => {
   console.log("SERVER IS RUNNING");
@@ -28,3 +30,4 @@ app.use("/jobs", jobs);
 app.use("/qualfications", qualfications);
 app.use("/requests", requests);
 app.use("/applicant", applicants);
+app.use("/ApplicantsM", ApplicantsM);
